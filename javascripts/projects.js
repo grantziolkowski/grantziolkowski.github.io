b19066d25ded7b9770000c37aff75aaa81abd854
grantziolkowski <grant.ziolkowski@gmail.com>
2015-06-29 17:49:48 -0400

    Remove unitialized submodule

--- javascripts/projects.js
+++ javascripts/projects.js
@@ -6,7 +6,10 @@ $(document).ready(function(){
     $(".summary").css({
       "opacity": "1",
        "-webkit-transform": "rotate(360deg)",
-      "transform": "rotate(360deg)"})
+       "-moz-webkit-transform": "rotate(360deg)",
+       "-ms-transform": "rotate(360deg)",
+       "-o-webkit-transform": "rotate(360deg)",
+       "transform": "rotate(360deg)"})
   }, function(){
       $(event.target).css({
       "opacity" : "1"
@@ -14,6 +17,9 @@ $(document).ready(function(){
     $(".summary").css({
       "opacity": "0",
        "-webkit-transform": "none",
+       "-moz-webkit-transform": "none",
+       "-ms-webkit-transform": "none",
+       "-o-webkit-transform": "none",
       "transform": "none"
     })
   })
